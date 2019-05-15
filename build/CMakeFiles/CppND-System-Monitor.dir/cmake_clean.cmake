619202ebb75ce208dbb6d24083b0ef044280a494
derekgui <derek.guiz@gmail.com>
2019-05-15 18:07:25 +1200

    restruct the project and add a cmake file to organise it.

---
+++ build/CMakeFiles/CppND-System-Monitor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppND-System-Monitor.dir/src/main.cpp.o"
+  "../bin/CppND-System-Monitor.pdb"
+  "../bin/CppND-System-Monitor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppND-System-Monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
